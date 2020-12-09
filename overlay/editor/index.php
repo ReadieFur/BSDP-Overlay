efe0be966820cd36e4b4a5fe022ab8faed4a3e84
Tristan Read <kof.readie@outlook.vom>
2020-12-09 14:10:20 +0000

    update .gitignore
    Added *.gi.* to exclude files from github (gi = gitignore)

---
+++ overlay/editor/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BSDP-Overlay | Editor</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7cOpen+Sans:400,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <script type="module" src="./assets/js/index.ts.js"></script>
+</head>
+<body>
+    <table>
+        <tbody>
+            <tr>
+                <td class="editorContainer">
+                    <table>
+                        <tbody>
+                            <tr class="titleContainer">
+                                <td><h3 class="center text">BSDP-Overlay<br>Editor</h3></td>
+                            </tr>
+                            <tr class="elementContainer">
+                                <td>
+                                    <div>
+                                        
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr class="positionContainer">
+                                <td>
+                                    <div>
+                                        <hr>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td class="overlayContainer">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
