f29e0d837bbf3551a15e5ea067ff70376e369ffa
kOF.Readie <tristan.read1508@gmail.com>
2021-02-24 22:26:49 +0000

    Created a quick way to test and preview elements

---
+++ src/assets/overlay_elements/index.php
@@ -0,0 +1,26 @@
+<?php
+    $title = 'BSDP Overlay | Editor';
+
+    global $WEB_ROOT;
+    global $SITE_ROOT;
+    $DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
+    require_once "$DOCUMENT_ROOT/roots.php";
+    require_once "$SITE_ROOT/assets/php/main.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php echo execAndRead("{$SITE_ROOT}/assets/php/head.php"); ?>
+    <link href="./overlay_elements.css" rel="stylesheet">
+    <script src="./overlay_elements.js" type="module"></script>
+</head>
+<header id="header"><?php echo execAndRead("{$SITE_ROOT}/assets/php/header.php"); ?></header>
+<body>
+    <span class="slideMenu"></span>
+    <!--Example URL: http://readie.global-gaming.localhost/bsdp-overlay/assets/overlay_elements/?element=round_bar/time/01-->
+    <div id="overlay">
+        <div id="element_01" class="elementContainer"></div>
+    </div>
+</body>
+<footer id="footer"></footer>
+</html>
