e00f24e087545e50e4c00db7ec565381357b54e3
kOF.Readie <tristan.read1508@gmail.com>
2021-03-04 13:16:37 +0000

    Update browser.ts

--- src/browser/browser.ts
+++ src/browser/browser.ts
@@ -2,113 +2,118 @@ import { Main } from "../assets/js/main";
 
 class Browser
 {
-    private searchBox!: HTMLFormElement;
+    private overlays: HTMLTableElement;
+    private pages: HTMLDivElement;
+    private search: HTMLFormElement;
 
     constructor()
     {
         new Main();
-        window.addEventListener("DOMContentLoaded", () => { this.DOMContentLoadedEvent(); });
-        window.addEventListener("load", () => { this.WindowLoadEvent(); });
-    }
+        this.overlays = Main.ThrowIfNullOrUndefined(document.querySelector("#overlays"));
+        this.pages = Main.ThrowIfNullOrUndefined(document.querySelector("#pages"));
+        this.search = Main.ThrowIfNullOrUndefined(document.querySelector("#search"));
 
-    private DOMContentLoadedEvent()
-    {
-        this.searchBox = Main.ThrowIfNullOrUndefined(document.querySelector("#search"));
-        this.searchBox.addEventListener("submit", (ev: Event) => { this.CustomSearch(ev); });
-    }
-
-    private WindowLoadEvent()
-    {
+        this.search.addEventListener("submit", (ev: Event) => { this.CustomSearch(ev); });
         this.GetOverlays(Main.urlParams.has("q") ? JSON.parse(decodeURIComponent(Main.urlParams.get("q")!)) : {page: 1});
     }
 
-    //Update this query type
+    //Rewrite this (piggybacking off of my old code).
+    //Update this query type.
     private GetOverlays(query: { [key: string]: string | number })
     {
-        let queryKey = Object.keys(query)[1];
-        let pageOnly = queryKey == null || queryKey == "undefined";
+        var queryKey: string = Object.keys(query)[1];
+        var pageOnly: boolean = queryKey == null || queryKey == "undefined";
         if (pageOnly) { delete query[queryKey]; }
-        let queryString = JSON.stringify(query);
+        var queryString = JSON.stringify(query);
 
-        $.ajax(
+        jQuery.ajax(
         {
-            url: `${Main.WEB_ROOT}/assets/php/getOverlays.php`,
-            data: `q=${encodeURIComponent(queryString)}`,
-            type: "GET",
+            url: `${Main.WEB_ROOT}/assets/php/overlay.php`,
+            data:
+            {
+                "q": JSON.stringify(
+                {
+                    method: "List",
+                    data: query
+                })
+            },
+            type: "POST",
             dataType: "json",
             error: Main.ThrowAJAXJsonError,
-            success: (data: any) => { success(data); }
-        });
-
-        function success(data: any) //Rewrite this object so it is type safe
-        {
-            if (data.results != null)
+            //Rewrite this object so it is type safe.
+            success: (response: any) =>
             {
-                let firstPush: boolean = true;
-                let overlayList = Main.ThrowIfNullOrUndefined(document.querySelector("#overlays"));
-                let pagesContainer = Main.ThrowIfNullOrUndefined(document.querySelector("#pages"));
-
-                while (overlayList.childElementCount > 1) { overlayList.removeChild(overlayList.lastChild); }
-                while (pagesContainer.childElementCount > 0) { pagesContainer.removeChild(pagesContainer.lastChild); }
-
-                //Add a short description, ending with elipsis after X characters?
-                data.results.forEach((result: any) =>
+                if (response.error === null)
                 {
-                    let tr = document.createElement("tr");
-                    let td1 = document.createElement("td");
-                    let td2 = document.createElement("td");
-                    let td3 = document.createElement("td");
-                    let b64 = document.createElement("img");
-                    let oname = document.createElement("a");
-                    let creator = document.createElement("a");
+                    var firstPush: boolean = true;
 
-                    b64.src = "data:image/png;base64," + btoa(
-                        new Uint8Array(result.b64)
-                        .reduce((bytedata, byte) => bytedata + String.fromCharCode(byte), '')
-                    );
-                    oname.innerHTML = result.oname;
-                    oname.href = `../overlay/?id=${result.id}`;
-                    creator.innerHTML = result.username;
-                    creator.href = `../user/?unid=${result.unid}`;
-        
-                    td1.appendChild(b64);
-                    td2.appendChild(oname);
-                    td3.appendChild(creator);
-                    tr.appendChild(td1);
-                    tr.appendChild(td2);
-                    tr.appendChild(td3);
-                    overlayList.appendChild(tr);
-                });
-            
-                let bc = 1;
-                let pages = Math.ceil(data.totalResults / 15);
-                while (bc <= pages)
-                {
-                    let pageBtn: HTMLButtonElement = document.createElement("button");
-                    pageBtn.innerHTML = bc.toString();
-                    pageBtn.className = "hollowButton";
-                    if (bc == query.page) { pageBtn.classList.add("hover"); }
-                    pageBtn.style.marginLeft = "10px";
-                    let btnFunction = pageOnly ? `getOverlays({page: ${bc}})`: `getOverlays({page: ${bc}, ${queryKey}: "${query[queryKey]}"})`;
-                    pageBtn.setAttribute("onclick", btnFunction);
-                    pagesContainer.appendChild(pageBtn);
-                    bc++;
+                    while (this.overlays.childElementCount > 1) { this.overlays.removeChild(this.overlays.lastChild!); }
+                    while (this.pages.childElementCount > 0) { this.pages.removeChild(this.pages.lastChild!); }
+
+                    //Add a short description, ending with elipsis after X characters?
+                    response.data.forEach((result: any) =>
+                    {
+                        var tr: HTMLTableRowElement = document.createElement("tr");
+                        var td1: HTMLTableDataCellElement = document.createElement("td");
+                        var td2: HTMLTableDataCellElement = document.createElement("td");
+                        var td3: HTMLTableDataCellElement = document.createElement("td");
+                        var thumbnail: HTMLImageElement = document.createElement("img");
+                        var name: HTMLAnchorElement = document.createElement("a");
+                        var creator: HTMLAnchorElement = document.createElement("a");
+
+                        thumbnail.src = result.data.data.thumbnail;
+                        name.innerHTML = result.data.oname;
+                        creator.innerHTML = result.data.username;
+                        tr.onclick = () => { window.location.pathname = `${Main.WEB_ROOT}/preview/${result.data.id}`; };
+                        
+                        td1.appendChild(thumbnail);
+                        td2.appendChild(name);
+                        td3.appendChild(creator);
+                        tr.appendChild(td1);
+                        tr.appendChild(td2);
+                        tr.appendChild(td3);
+                        this.overlays.appendChild(tr);
+                    });
+                
+                    var bc: number = 1;
+                    var pages: number = Math.ceil(response.totalResults / 15);
+                    while (bc <= pages)
+                    {
+                        var pageBtn: HTMLButtonElement = document.createElement("button");
+                        pageBtn.innerHTML = bc.toString();
+                        pageBtn.className = "hollowButton";
+                        if (bc == query.page) { pageBtn.classList.add("hover"); }
+                        pageBtn.style.marginLeft = "10px";
+                        pageBtn.onclick = () =>
+                        {
+                            if (pageOnly)
+                            { this.GetOverlays({page: bc}); }
+                            else
+                            { this.GetOverlays({page: bc, queryKey: query[queryKey]}); }
+                        };
+                        this.pages.appendChild(pageBtn);
+                        bc++;
+                    }
+
+                    if(!firstPush)
+                    {
+                        Main.urlParams.set("q", queryString);
+                        window.history.pushState(queryString, `Browser | BSDP Overlay`, "?" + Main.urlParams.toString());
+                    }
+                    else { firstPush = false; }
                 }
-
-                if(!firstPush)
+                else
                 {
-                    Main.urlParams.set("q", queryString);
-                    window.history.pushState(queryString, `BSDP Overlay | Browser`, "?" + Main.urlParams.toString());
+                    //Error.
                 }
-                else { firstPush = false; }
             }
-        }
+        });
     }
 
     private CustomSearch(ev: Event)
     {
         ev.preventDefault();
-        let searchQuery: string = (<HTMLInputElement>this.searchBox.firstElementChild!).value; //Get if page is defined
+        var searchQuery: string = (<HTMLInputElement>this.search.firstElementChild!).value; //Get if page is defined
         if (searchQuery.includes(":"))
         {
             var searchQuerySplit = searchQuery.split(":");
