d23635c235d196264df029593a74766310fbe77c
kOF.Readie <tristan.read1508@gmail.com>
2021-03-04 13:17:49 +0000

    Added overlay loading

--- src/view/index.php
+++ src/view/index.php
@@ -1,20 +1,40 @@
 <?php
+    $title = 'View | BSDP Overlay';
+
     $WEB_ROOT;
     $SITE_ROOT;
     $DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
     require_once "$DOCUMENT_ROOT/roots.php";
     require_once "$SITE_ROOT/assets/php/main.php";
 
+    $REQUEST_SCHEME = $_SERVER["REQUEST_SCHEME"];
+    $HTTP_HOST = $_SERVER["HTTP_HOST"];
+
     $page;
     $splitPath = array_filter(explode('/', $REQUEST_URI));
-    $overlayID = $splitPath[count($splitPath)];
+    $idPosition = array_search('view', $splitPath);
 
-    if (strlen($overlayID) == 13) { $page = execAndRead("../edit/index.php"); }
-    else //Default overlay
-    {
-        $REQUEST_SCHEME = $_SERVER["REQUEST_SCHEME"];
-        $HTTP_HOST = $_SERVER["HTTP_HOST"];
-        header("Location: $REQUEST_SCHEME://$HTTP_HOST$WEB_ROOT/view/default/", true, 301);
-    }
-    echo $page;
+    if (!(strlen($splitPath[$idPosition + 1]) === 13 && count($splitPath) === $idPosition + 1))
+    { header("Location: $REQUEST_SCHEME://$HTTP_HOST$WEB_ROOT/view/default/", true, 302); }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php echo execAndRead("{$SITE_ROOT}/assets/php/head.php"); ?>
+    <link rel="stylesheet" href="<?php echo $WEB_ROOT; ?>/view/view.css">
+    <script src="<?php echo $WEB_ROOT; ?>/view/view.js" type="module"></script>
+</head>
+<header id="header"><?php echo execAndRead("{$SITE_ROOT}/assets/php/header.php"); ?></header>
+<body>
+    <span class="slideMenu"></span>
+    <span id="splashScreen">
+        <div class="center">
+            <h2 id="ssHeader">BSDP Overlay</h2>
+            <img class="medium" alt="A missing picture of me ;)" src="https://cdn.global-gaming.co/images/team/members/readiecircle.png">
+            <h3 id="ssSubHeader">Loading...</h3>
+        </div>
+    </span>
+    <div id="overlay"></div>
+</body>
+<footer id="footer"></footer>
+</html>
---
+++ src/view/view.ts
@@ -0,0 +1,110 @@
+import { Main } from "../assets/js/main";
+import { HeaderSlide } from "../assets/js/headerSlide";
+import { OverlayPOSTResponse, SavedElements, UI } from "../assets/js/overlay/ui";
+import { Client } from "../assets/js/overlay/client";
+
+class View
+{
+    private ui!: UI;
+    private client!: Client;
+
+    public async Init(): Promise<View>
+    {
+        new Main();
+        new HeaderSlide();
+
+        this.ui = Main.ThrowIfNullOrUndefined(await new UI().Init());
+
+        this.client = new Client(Main.urlParams.get("ip"));
+        this.client.AddEndpoint("MapData");
+        this.client.websocketData["MapData"].e.addListener("message", (data) => { this.ui.UpdateMapData(data); });
+        this.client.AddEndpoint("LiveData");
+        this.client.websocketData["LiveData"].e.addListener("message", (data) => { this.ui.UpdateLiveData(data); });
+
+        await this.LoadOverlay();
+
+        //Hide splash screen.
+        let splashScreen: HTMLDivElement = Main.ThrowIfNullOrUndefined(document.querySelector("#splashScreen"));
+        splashScreen.style.opacity = "0";
+        setTimeout(() => { splashScreen!.style.display = "none"; }, 400);
+
+        return this;
+    }
+
+    private async LoadOverlay(): Promise<void>
+    {
+        var path: string[] = window.location.pathname.split("/");
+        var response: OverlayPOSTResponse = await jQuery.ajax(
+        {
+            type: "POST",
+            url: `${Main.WEB_ROOT}/assets/php/overlay.php`,
+            dataType: "json",
+            error: Main.ThrowAJAXJsonError,
+            data:
+            {
+                "q": JSON.stringify(
+                {
+                    method: "GetByID",
+                    data:
+                    {
+                        id: path[path.length - 1]
+                    }
+                })
+            }
+        });
+
+        if (response.error === null)
+        {
+            var elements: SavedElements = response.data.elements;
+
+            //TMP
+            if (parseInt(response.data.isPrivate) === 1 && response.data.unid !== Main.RetreiveCache("READIE-UI"))
+            {
+                //User does not have permission to view the overlay.
+                window.location.pathname = `${Main.WEB_ROOT}/browser/`;
+            }
+
+            for (const category of Object.keys(elements))
+            {
+                for (const type of Object.keys(elements[category]))
+                {
+                    for (const id of Object.keys(elements[category][type]))
+                    {
+                        for (let i = 0; i < elements[category][type][id].length; i++)
+                        {
+                            const elementProperties = elements[category][type][id][i];
+                            var container: HTMLDivElement = this.ui.CreateElement(category, type, id);
+                            if (elementProperties.position.top !== undefined)
+                            {
+                                container.style.top = elementProperties.position.top;
+                                container.style.height = `${elementProperties.height}px`;
+                            }
+                            else
+                            {
+                                container.style.height = `${elementProperties.height}px`;
+                                container.style.bottom = elementProperties.position.bottom!;
+                            }
+                            if (elementProperties.position.left !== undefined)
+                            {
+                                container.style.left = elementProperties.position.left;
+                                container.style.width = `${elementProperties.width}px`;
+                            }
+                            else
+                            {
+                                container.style.width = `${elementProperties.width}px`;
+                                container.style.right = elementProperties.position.right!;
+                            }
+                            this.ui.overlay.appendChild(container);
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            //INVALID_SQL_RESPONSE || MISSING_PARAMETERS || INVALID_PERMISSIONS || INVALID_CREDENTIALS.
+            window.location.pathname = `${Main.WEB_ROOT}/browser/`;
+        }
+    }
+}
+new View().Init();
