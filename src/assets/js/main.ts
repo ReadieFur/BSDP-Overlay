603f5486a288560bda037b471b775cf5ee9e8456
kOF.Readie <kof.readie@outlook.com>
2021-04-25 22:00:03 +0100

    Update main to talk with the account IFrame

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -7,6 +7,7 @@ export class Main
     public static footer: HTMLElement;
     public static urlParams: URLSearchParams;
     public static zoom: number;
+    public static accountContainer: HTMLIFrameElement;
 
     constructor()
     {
@@ -14,17 +15,22 @@ export class Main
         Main.urlParams = new URLSearchParams(location.search);
         Main.header = Main.ThrowIfNullOrUndefined(document.querySelector("#header"));
         Main.footer = Main.ThrowIfNullOrUndefined(document.querySelector("#footer"));
-
+        Main.accountContainer = Main.ThrowIfNullOrUndefined(Main.header.querySelector("#accountContainer"));
+        
+        window.addEventListener("message", (ev) => { this.WindowMessageEvent(ev); });
         window.addEventListener("resize", () => { this.WindowResizeEvent(); });
+        Main.ThrowIfNullOrUndefined(document.querySelector("#accountButton")).addEventListener("click", () => { Main.AccountMenuToggle(true); });
         this.WindowResizeEvent();
 
-        if (Main.RetreiveCache("READIE-DARK") != "false") { Main.DarkTheme(true); }
+        if (Main.RetreiveCache("READIE_DARK") != "false") { Main.DarkTheme(true); }
         else { Main.DarkTheme(false); }
         Main.ThrowIfNullOrUndefined(document.querySelector("#darkMode")).addEventListener("click", () =>
         {
-            var cachedValue = Main.RetreiveCache("READIE-DARK");
+            var cachedValue = Main.RetreiveCache("READIE_DARK");
             if (cachedValue == undefined || cachedValue == "false") { Main.DarkTheme(true); }
             else { Main.DarkTheme(false); }
+            //CBA to do the dynamic url thing I normally do, nothing sensitive is being sent over anyway.
+            if (Main.accountContainer.contentWindow != null) { Main.accountContainer.contentWindow.postMessage("UPDATE_THEME", "*" /*Main.accountContainer.contentWindow?.location.href*/ /*Main.accountContainer.src*/); }
         });
 
         this.HighlightActivePage();
@@ -67,6 +73,68 @@ export class Main
         });
     }
 
+    private WindowMessageEvent(ev: MessageEvent<any>): void
+    {
+        var host = window.location.host.split('.');
+        if (ev.origin.split('/')[2] == `api-readie.global-gaming.${host[host.length - 1]}`)
+        {
+            if (Main.TypeOfReturnData(ev.data))
+            {
+                if (ev.data.error)
+                {
+                    //Alert error.
+                    Main.AccountMenuToggle(false);
+                }
+                else if (typeof(ev.data.data) === "string")
+                {
+                    switch (ev.data.data)
+                    {
+                        case "BACKGROUND_CLICK":
+                            Main.AccountMenuToggle(false);
+                            break;
+                        case "LOGGED_IN":
+                            Main.AccountMenuToggle(false);
+                            break;
+                        case "LOGGED_OUT":
+                            window.location.reload();
+                            break;
+                        case "ACCOUNT_DELETED":
+                            window.location.reload();
+                            break;
+                        default:
+                            //Not implemented.
+                            break;
+                    }
+                }
+                else
+                {
+                    //Alert unknown error/response.
+                    console.log("Unknown response: ", ev);
+                    Main.AccountMenuToggle(false);
+                }
+            }
+            else
+            {
+                //Alert unknown error/response.
+                console.log("Unknown response: ", ev);
+                Main.AccountMenuToggle(false);
+            }
+        }
+    }
+
+    public static AccountMenuToggle(show: boolean)
+    {
+        if (show) { if (Main.accountContainer.contentWindow != null) { Main.accountContainer.contentWindow.postMessage("UPDATE_THEME", "*"); } Main.accountContainer.style.display = "block"; }
+        Main.accountContainer.classList.remove(show ? "fadeOut" : "fadeIn");
+        Main.accountContainer.classList.add(show ? "fadeIn" : "fadeOut");
+        if (!show) { setTimeout(() => { Main.accountContainer.style.display = "none"; }, 399); }
+    }
+
+    public static TypeOfReturnData(data: any): data is ReturnData
+    {
+        return (data as ReturnData).error !== undefined && (data as ReturnData).data !== undefined;
+    }
+
     public static ThrowIfNullOrUndefined(variable: any): any
     {
         if (variable === null || variable === undefined) { throw new TypeError(`${variable} is null or undefined`); }
@@ -75,7 +143,7 @@ export class Main
 
     public static DarkTheme(dark: boolean): void
     {
-        Main.SetCache("READIE-DARK", dark ? "true" : "false", 365);
+        Main.SetCache("READIE_DARK", dark ? "true" : "false", 365);
         var darkButton: HTMLInputElement = Main.ThrowIfNullOrUndefined(document.querySelector("#darkMode"));
         var themeColours: HTMLStyleElement = Main.ThrowIfNullOrUndefined(document.querySelector("#themeColours"));
         if (dark) { darkButton.classList.add("accentText"); }
@@ -108,8 +176,8 @@ export class Main
 
     public static SetCache(cookie_name: string, value: string, time: number, path: string = '/'): void
     {
-        var hostSplit = window.location.host.split("."); //Just for localhost testing
-        var domain = `readie.global-gaming.${hostSplit[hostSplit.length - 1]}`; 
+        var hostSplit = window.location.host.split(".");
+        var domain = `.${hostSplit[hostSplit.length - 2]}.${hostSplit[hostSplit.length - 1]}`;
         var expDate = new Date();
         expDate.setDate(expDate.getDate() + time);
         document.cookie = `${cookie_name}=${value}; expires=${expDate.toUTCString()}; path=${path}; domain=${domain};`;
@@ -123,4 +191,10 @@ export class Main
         //Make a UI for this
         window.alert(message);
     }
+}
+
+export interface ReturnData
+{
+    error: boolean,
+    data: any
 }
