bccba911a261c20ceae31283c459b6592fe83277
kOF.Readie <kof.readie@outlook.com>
2021-04-25 22:01:06 +0100

    Load overlay preview

--- src/preview/index.php
+++ src/preview/index.php
@@ -4,30 +4,35 @@
     $DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
     require_once "$DOCUMENT_ROOT/roots.php";
     require_once "$SITE_ROOT/assets/php/main.php";
+    $title = 'Preview | BSDP Overlay';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php echo execAndRead("{$SITE_ROOT}/assets/php/head.php"); ?>
-    <link rel="stylesheet" href="preview.css">
-    <script src="preview.js" type="module"></script>
+    <link rel="stylesheet" href="<?php echo $WEB_ROOT; ?>/preview/preview.css">
+    <script src="<?php echo $WEB_ROOT; ?>/preview/preview.js" type="module"></script>
 </head>
 <header id="header"><?php echo execAndRead("{$SITE_ROOT}/assets/php/header.php"); ?></header>
 <body>
     <section>
-        <table>
-            <tr>
-                <td class="overlayImageContainer"><img id="overlayImage" src="" alt="Couldnt load preview"></td>
-                <td class="textContainer">
-                    <div>
-                        <h3 id="overlayName">Overlay Name</h3>
-                        <p id="overlayDescription">Overlay Description</p>
-                        <button id="overlayButton" class="hollowButton">Use Overlay</button>
-                        <span>Creator: <a id="overlayCreator">Overlay Creator</a></span>
-                        <span>ID: <a id="overlayID">Overlay ID</a></span>
-                    </div>
-                </td>
-            </tr>
+        <table id="overlayDetailsContainer">
+            <tbody>
+                <tr>
+                    <td id="overlayImageContainer">
+                        <img id="thumbnail" alt="Couldnt load preview">
+                    </td>
+                    <td id="overlayTextContainer">
+                        <div>
+                            <h3 id="name">Overlay Name</h3>
+                            <p id="description">Overlay Description</p>
+                            <a id="overlayLink" class="asButton">Use Overlay</a>
+                            <span>Creator: <a id="username">Overlay Creator</a></span>
+                            <span>ID: <a id="id">Overlay ID</a></span>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </section>
 </body>
--- src/preview/preview.ts
+++ src/preview/preview.ts
@@ -1,53 +1,59 @@
-import { Main } from "../assets/js/main";
+import { Main, ReturnData } from "../assets/js/main";
+import { IOverlayData, OverlayHelper } from "../assets/js/overlay/overlayHelper";
 
 class Preview
 {
+    private overlayImageContainer: HTMLTableRowElement;
+    private thumbnail: HTMLImageElement;
+    private name: HTMLHeadingElement;
+    private description: HTMLParagraphElement;
+    private overlayLink: HTMLLinkElement;
+    private username: HTMLLinkElement;
+    private id: HTMLLinkElement;
+
     constructor()
     {
         new Main();
-        
-        if (!Main.urlParams.has("id"))
+
+        this.overlayImageContainer = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayImageContainer"));
+        this.thumbnail = Main.ThrowIfNullOrUndefined(document.querySelector("#thumbnail"));
+        this.name = Main.ThrowIfNullOrUndefined(document.querySelector("#name"));
+        this.description = Main.ThrowIfNullOrUndefined(document.querySelector("#description"));
+        this.overlayLink = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayLink"));
+        this.username = Main.ThrowIfNullOrUndefined(document.querySelector("#username"));
+        this.id = Main.ThrowIfNullOrUndefined(document.querySelector("#id"));
+
+        var path = window.location.pathname.split('/').filter((part) => { return part != ""; });
+        if (path[path.length - 1] === "preview" || path[path.length - 2] !== "preview" || path[path.length - 1].length != 13) { window.location.href = `${Main.WEB_ROOT}/browser/`; }
+
+        OverlayHelper.OverlayPHP({
+            method: "getOverlayByID",
+            data:
+            {
+                id: path[path.length - 1]
+            },
+            success: (response) => { this.GotOverlay(response); }
+        });
+    }
+
+    private GotOverlay(response: ReturnData)
+    {
+        if (response.error)
         {
-            window.location.replace("../browser/");
+            //Alert error.
+            window.location.href = `${Main.WEB_ROOT}/browser/`;
         }
 
-        if (Main.urlParams.has("id"))
-        {
-            let queryString = JSON.stringify({id: Main.urlParams.get("id")});
-            $.ajax(
-            {
-                url: "../getOverlays.php",
-                data: `q=${encodeURIComponent(queryString)}`,
-                type: "GET",
-                dataType: "json",
-                error: (err) => { console.log(err); },
-                success: sucess
-            });
-        
-            function sucess(data: any)
-            {
-                if (data.result != null)
-                {
-                    let overlayImage = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayImage"));
-                    let overlayName = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayName"));
-                    let overlayDescription = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayDescription"));
-                    let overlayButton = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayButton"));
-                    let overlayCreator = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayCreator"));
-                    let overlayID = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayID"));
-                    overlayImage.src = "data:image/png;base64," + btoa(
-                        new Uint8Array(data.result.b64)
-                        .reduce((bytedata, byte) => bytedata + String.fromCharCode(byte), '')
-                    );
-                    overlayName.innerHTML = data.result.oname;
-                    overlayDescription.innerHTML = data.result.comment;
-                    overlayButton.setAttribute("onclick", `window.location = "http://u-readie.global-gaming.co/bsdp-overlay/editor/?style=${data.result.id}";`);
-                    overlayCreator.innerHTML = data.result.username;
-                    overlayCreator.href = `../user/?unid=${data.result.unid}`;
-                    overlayID.innerHTML = data.result.id;
-                }
-                else { window.location.replace("../browser/"); }
-            }
-        }
+        var overlay = response.data as IOverlayData;
+        if (overlay.thumbnail === null) { this.overlayImageContainer.style.display = "none"; }
+        else { this.thumbnail.src = overlay.thumbnail; }
+        this.name.innerText = overlay.name;
+        this.description.innerText = overlay.description === null ? '' : overlay.description;
+        this.overlayLink.href = `${Main.WEB_ROOT}/view/${overlay.id}/`;
+        this.username.innerText = overlay.username;
+        this.username.href = `${Main.WEB_ROOT}/browser/#GENERATE_QUERY_HERE`;
+        this.id.innerText = overlay.id;
+        this.id.href = `${Main.WEB_ROOT}/view/${overlay.id}/`;
     }
 }
 new Preview();
