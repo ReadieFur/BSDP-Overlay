b4f63af24173597eaa38a16f93108a99475924f6
kOF.Readie <kof.readie@outlook.com>
2021-04-27 01:16:42 +0100

    Update preview + added edit/delete buttons

--- src/preview/index.php
+++ src/preview/index.php
@@ -26,9 +26,25 @@
                         <div>
                             <h3 id="name">Overlay Name</h3>
                             <p id="description">Overlay Description</p>
-                            <a id="overlayLink" class="asButton">Use Overlay</a>
-                            <span>Creator: <a id="username">Overlay Creator</a></span>
-                            <span>ID: <a id="id">Overlay ID</a></span>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <div class="joinButtons">
+                                                <button id="overlayLinkContainer" class="containsLink ignore"><a id="overlayLink">Use Overlay</a></button>
+                                                <!--Id like to have these two (^v) as an 'a' element but my css wont allow for it, and I can't be arsed to change it.-->
+                                                <!--So instead I just put the 'a' element inside the button with a midified button/a hitbox (padding).-->
+                                                <button id="editLinkContainer" class="containsLink"><a id="editLink">Edit</a></button>
+                                                <button id="deleteButton">Delete</button>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <span>Creator: <a id="username" class="light">Overlay Creator</a></span>
+                                            <span>ID: <a id="id" class="light">Overlay ID</a></span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </td>
                 </tr>
--- src/preview/preview.ts
+++ src/preview/preview.ts
@@ -3,15 +3,25 @@ import { IOverlayData, OverlayHelper } from "../assets/js/overlay/overlayHelper"
 
 class Preview
 {
-    private overlayImageContainer: HTMLTableRowElement;
-    private thumbnail: HTMLImageElement;
-    private name: HTMLHeadingElement;
-    private description: HTMLParagraphElement;
-    private overlayLink: HTMLLinkElement;
-    private username: HTMLLinkElement;
-    private id: HTMLLinkElement;
+    
+    private overlayImageContainer!: HTMLTableRowElement;
+    private thumbnail!: HTMLImageElement;
+    private name!: HTMLHeadingElement;
+    private description!: HTMLParagraphElement;
+    private overlayLink!: HTMLLinkElement;
+    private overlayLinkContainer!: HTMLButtonElement;
+    private editLink!: HTMLLinkElement;
+    private editLinkContainer!: HTMLButtonElement;
+    private deleteButton!: HTMLButtonElement;
+    private username!: HTMLLinkElement;
+    private id!: HTMLLinkElement;
 
-    constructor()
+    private path!: string[];
+    private overlayID?: string;
+
+    private deleteAlerted = false;
+
+    public async Init()
     {
         new Main();
 
@@ -20,40 +30,138 @@ class Preview
         this.name = Main.ThrowIfNullOrUndefined(document.querySelector("#name"));
         this.description = Main.ThrowIfNullOrUndefined(document.querySelector("#description"));
         this.overlayLink = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayLink"));
+        this.overlayLinkContainer = Main.ThrowIfNullOrUndefined(document.querySelector("#overlayLinkContainer"));
+        this.editLink = Main.ThrowIfNullOrUndefined(document.querySelector("#editLink"));
+        this.editLinkContainer = Main.ThrowIfNullOrUndefined(document.querySelector("#editLinkContainer"));
+        this.deleteButton = Main.ThrowIfNullOrUndefined(document.querySelector("#deleteButton"));
         this.username = Main.ThrowIfNullOrUndefined(document.querySelector("#username"));
         this.id = Main.ThrowIfNullOrUndefined(document.querySelector("#id"));
 
-        var path = window.location.pathname.split('/').filter((part) => { return part != ""; });
-        if (path[path.length - 1] === "preview" || path[path.length - 2] !== "preview" || path[path.length - 1].length != 13) { window.location.href = `${Main.WEB_ROOT}/browser/`; }
+        window.addEventListener("message", (ev) => { this.WindowMessageEvent(ev); })
 
+        this.path = window.location.pathname.split('/').filter((part) => { return part != ""; });
+        if (this.path[this.path.length - 1] === "preview" || this.path[this.path.length - 2] !== "preview" || this.path[this.path.length - 1].length != 13)
+        {
+            Main.Alert("Invalid ID.");
+            await Main.Sleep(1000);
+            window.location.href = `${Main.WEB_ROOT}/browser/`;
+        }
+
+        this.deleteButton.addEventListener("click", (ev) => { this.DeleteButtonClickEvent(ev); });
+
+        this.GetOverlay();
+    }
+
+    private GetOverlay()
+    {
         OverlayHelper.OverlayPHP({
             method: "getOverlayByID",
             data:
             {
-                id: path[path.length - 1]
+                id: this.path[this.path.length - 1]
             },
             success: (response) => { this.GotOverlay(response); }
         });
     }
 
-    private GotOverlay(response: ReturnData)
+    private async GotOverlay(response: ReturnData)
     {
         if (response.error)
         {
-            //Alert error.
+            console.error(response);
+            Main.Alert(Main.GetPHPErrorMessage(response.data));
+            await Main.Sleep(1000);
             window.location.href = `${Main.WEB_ROOT}/browser/`;
         }
 
         var overlay = response.data as IOverlayData;
         if (overlay.thumbnail === null) { this.overlayImageContainer.style.display = "none"; }
         else { this.thumbnail.src = overlay.thumbnail; }
+
+        this.overlayID = overlay.id;
+
+        if (Main.RetreiveCache("READIE_UID") === overlay.uid)
+        {
+            this.overlayLinkContainer.classList.remove("ignore");
+            this.editLinkContainer.style.display = "block";
+            this.deleteButton.style.display = "block";
+        }
+        else
+        {
+            this.overlayLinkContainer.classList.add("ignore");
+            this.editLinkContainer.style.display = "none";
+            this.deleteButton.style.display = "none";
+        }
+
         this.name.innerText = overlay.name;
         this.description.innerText = overlay.description === null ? '' : overlay.description;
         this.overlayLink.href = `${Main.WEB_ROOT}/view/${overlay.id}/`;
+        this.editLink.href = `${Main.WEB_ROOT}/edit/${overlay.id}/`;
         this.username.innerText = overlay.username;
         this.username.href = `${Main.WEB_ROOT}/browser/#GENERATE_QUERY_HERE`;
         this.id.innerText = overlay.id;
         this.id.href = `${Main.WEB_ROOT}/view/${overlay.id}/`;
     }
+
+    //Look into ckecking if the user clicked elsewhere to then re-enable the saftey alert.
+    private DeleteButtonClickEvent(ev: Event)
+    {
+        if (this.overlayID === null)
+        {
+            Main.Alert("Overlay ID not found.");
+        }
+        else
+        {
+            if (!this.deleteAlerted)
+            {
+                Main.Alert("Are you sure you want to delete this overlay?<br><small>This cannot be undone.<br>Click the button again to delete the overlay.</small>");
+                this.deleteAlerted = true;
+            }
+            else
+            {
+                OverlayHelper.OverlayPHP(
+                {
+                    method: "deleteOverlay",
+                    data:
+                    {
+                        id: this.overlayID
+                    },
+                    success: async (response) =>
+                    {
+                        if (response.error)
+                        { Main.Alert(Main.GetPHPErrorMessage(response.data)); }
+                        else if (response.data !== true)
+                        { Main.Alert(Main.GetPHPErrorMessage(response.data)); }
+                        else
+                        {
+                            Main.Alert("Overlay successfully deleted.");
+                            await Main.Sleep(1000);
+                            window.location.href = `${Main.WEB_ROOT}/browser/`;
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private WindowMessageEvent(ev: MessageEvent<any>)
+    {
+        var host = window.location.host.split('.');
+        if (ev.origin.split('/')[2] == `api-readie.global-gaming.${host[host.length - 1]}`)
+        {
+            if (Main.TypeOfReturnData(ev.data))
+            {
+                switch (ev.data.data)
+                {
+                    case "LOGGED_IN":
+                        this.GetOverlay();
+                        break;
+                    default:
+                        //Not implemented.
+                        break;
+                }
+            }
+        }
+    }
 }
-new Preview();
+new Preview().Init();
