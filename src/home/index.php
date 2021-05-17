9b3ed0fcd3bd0caddac82566ea7812a5d45bc5b6
kOF.Readie <kof.readie@outlook.com>
2021-05-17 21:32:58 +0100

    Added fixed video on scroll (instead of the picture before)

--- src/home/index.php
+++ src/home/index.php
@@ -18,14 +18,25 @@
 </head>
 <header id="header"><?php echo execAndRead("{$SITE_ROOT}/assets/php/header.php"); ?></header>
 <body>
-    <span id="splash">
+    <div class="splashText">
         <div class="center">
             <h1 class="center x text">BSDP Overlay</h1>
             <p class="center x text">The official overlay tool for BSDataPuller</p>
         </div>
-    </span>
-    <br>
-    <section id="about" class="markdown"></section>
+    </div>
+    <div id="splashVideoAndAbout">
+        <div class="fixedBackground">
+            <div class="background">
+                <video src="http://www.w3schools.com/html/mov_bbb.mp4" autoplay muted loop></video>
+            </div>
+            <div class="contentAfter">
+                <div>
+                    <br>
+                    <section id="about" class="markdown"></section>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 <footer id="footer"><?php echo execAndRead("{$SITE_ROOT}/assets/php/footer.php"); ?></footer>
 </html>
