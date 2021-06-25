8be63dc6fa8bfa2ea4bf6d8c9aa00159286225f9
Tristan Read <60425965+kOFReadie@users.noreply.github.com>
2021-06-25 22:05:31 +0100

    Update closed_release_access_manager.php

--- src/assets/php/closed_release_access_manager.php
+++ src/assets/php/closed_release_access_manager.php
@@ -3,7 +3,8 @@
 
 $allowedUsers = array(
     '60d1de6254a82537157312', //readie - localhost
-    '6098449426bca059481523' //readie
+    '6098449426bca059481523', //readie
+    '60d6433d807c2825942598', //unskilledfreak
 );
 
 if (!in_array($_COOKIE['READIE_UID']??null, $allowedUsers))
