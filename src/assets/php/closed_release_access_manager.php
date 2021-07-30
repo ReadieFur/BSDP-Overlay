460413b542f98a5dc253b0a26f06bc996936f475
Tristan Read <60425965+kOFReadie@users.noreply.github.com>
2021-07-30 18:04:24 +0100

    Misc changes

---
+++ src/assets/php/closed_release_access_manager.php
@@ -0,0 +1,20 @@
+<?php
+//This file will be edited manually and I have not made it a database check because it is only a temporary thing.
+
+$allowedUsers = array(
+    '60d1de6254a82537157312', //readie - localhost
+    '60fad16b06c78865178484', //readiealt - localhost
+    '6098449426bca059481523', //readie
+    '60d2968705af7717659797', //remy
+    '60d6433d807c2825942598', //unskilledfreak
+    '60db9eafd4559818088340', //pixelboom
+    '60dd6af2547a0534874813', //charalol
+    '60ff2e7f0e478823584208', //aetherialflow
+);
+
+if (!in_array($_COOKIE['READIE_UID']??null, $allowedUsers))
+{
+    http_response_code(401);
+    echo "401 Unauthorized";
+    exit;
+}
