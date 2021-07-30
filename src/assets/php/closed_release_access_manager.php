f41fe6ff34329bd56e9d13052dfa4604afbc83fb
Tristan Read <60425965+kOFReadie@users.noreply.github.com>
2021-07-30 20:48:16 +0100

    Update closed_release_access_manager.php

--- src/assets/php/closed_release_access_manager.php
+++ src/assets/php/closed_release_access_manager.php
@@ -10,7 +10,8 @@ $allowedUsers = array(
     '60db9eafd4559818088340', //pixelboom
     '60dd6af2547a0534874813', //charalol
     '60ff2e7f0e478823584208', //aetherialflow
-    '610453d779844521709098', //Laco
+    '610453d779844521709098', //laco
+    '610456af0f4f7662694321', //dragonplays
 );
 
 if (!in_array($_COOKIE['READIE_UID']??null, $allowedUsers))
